a7dc04bcc63d33fc0e303450848ee7f5798e9307
pauldapps <59803295+pauldapps@users.noreply.github.com>
2021-09-12 14:11:24 -0400

    Update new_passphrase.py

--- new_passphrase.py
+++ new_passphrase.py
@@ -1,8 +1,15 @@
+"""
+words = the number of words you would like to compromise each passphrase of.
+num = the number of passphrase you would like to generate.
+dice_rolls = corrosponds to the word list. The number of times for each word you need to roll the dice inorder to look up a word in the list.
+dice_sides = corrosponds to the word list. The original and most others are designed for standard 6 sided dice.
+list_url = the url where the list can be found in two columns, tab delimited format.
+"""
 import random
 import requests
 
-def new_passphrase(words: int=4,num: int=1,seperator: str='-',dice_rolls: int=4, dice_sides: int=6, eff_url: str="https://www.eff.org/files/2016/09/08/eff_short_wordlist_1.txt"):
-    data = (requests.get(eff_url)).text.splitlines()
+def new_passphrase(words: int=4,num: int=1,seperator: str='-',dice_rolls: int=4, dice_sides: int=6, list_url: str="https://www.eff.org/files/2016/09/08/eff_short_wordlist_1.txt"):
+    data = (requests.get(list_url)).text.splitlines()
     eff_list = []
     for l in data:
         eff_list.append(l.split('\t'))
