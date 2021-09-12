a1e140e7ff2c5f5bcc68f09da7d8899b9e203a06
pauldapps <59803295+pauldapps@users.noreply.github.com>
2021-09-12 14:02:22 -0400

    Update new_passphrase.py

--- new_passphrase.py
+++ new_passphrase.py
@@ -2,8 +2,7 @@ import random
 import requests
 
 def new_passphrase(words=4,num=1,seperator='-',dice_sides=4):
-    data = requests.get("https://www.eff.org/files/2016/09/08/eff_short_wordlist_1.txt")
-    data = data.text.splitlines()
+    data = (requests.get("https://www.eff.org/files/2016/09/08/eff_short_wordlist_1.txt")).text.splitlines()
     eff_list = []
     for l in data:
         eff_list.append(l.split('\t'))
