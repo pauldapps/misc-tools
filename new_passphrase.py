882849cc0ed11be08937951e1f909a92a5edbb6b
pauldapps <59803295+pauldapps@users.noreply.github.com>
2021-09-13 10:57:45 -0400

    Update new_passphrase.py

--- new_passphrase.py
+++ new_passphrase.py
@@ -1,5 +1,3 @@
-import random
-import requests
 def new_passphrase(words: int=4, num: int=1,seperator: str='-', dice_rolls: int=4, dice_sides: int=6, list_url: str="https://www.eff.org/files/2016/09/08/eff_short_wordlist_1.txt"):
     """Generates one or more secure passphrases.
 
@@ -20,7 +18,7 @@ def new_passphrase(words: int=4, num: int=1,seperator: str='-', dice_rolls: int=
     for l in data:
         eff_list.append(l.split('\t'))
     eff_list = dict(eff_list)
-    pass_out = ""
+    str_out = ""
     list_out = []
     for n in range(num):
         for w in range(words):
@@ -28,12 +26,12 @@ def new_passphrase(words: int=4, num: int=1,seperator: str='-', dice_rolls: int=
             for s in range(dice_rolls):
                 result += str(random.randint(1,dice_sides))   
             if w == 0:
-                pass_out += f'{eff_list[result]}'
+                str_out += f'{eff_list[result]}'
             else: 
-                pass_out += f'{seperator}{eff_list[result]}'
+                str_out += f'{seperator}{eff_list[result]}'
         if num == 1:
-            return pass_out
+            return str_out
         else:
-            list_out.append(pass_out)
-            pass_out = ""
+            list_out.append(str_out)
+            str_out = ""
     return list_out  
