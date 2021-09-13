dc59572f08fc0ccf7c2e5b7af39b5a23eaab40a2
pauldapps <59803295+pauldapps@users.noreply.github.com>
2021-09-13 11:44:16 -0400

    Update format_line_protocol.py

--- format_line_protocol.py
+++ format_line_protocol.py
@@ -1,11 +1,26 @@
-"""
-measurement = 'serverroom'
-tags = {'category': 'general', 'priority': 'high', 'owningParty': 'maintenace'}
-field = {'humidity': 0.99} #get_sensor_humidity
-format_line_protocol(measurement,field,tags)
-'serverroom,category=general,priority=high,owningParty=maintenace humidity=0.99'
-"""
 def format_line_protocol(measurement: str,field: dict,tags: dict={}) -> str:
+    """Converts input into influxDB line protocol format.
+
+    Args:
+        measurement (str): This is the overarching "thing" you're monitoring.
+        field (dict): This is the metric you're collecting for the "thing."
+        tags (dict, optional): These are optional other attributes you want to attach to the measurement. Defaults to {}.
+
+    Raises:
+        Exception: Multiple fields/metrics specified. Please format each field/metric separately.
+
+    Returns:
+        str: Line protocol formated string ready to be pushed to InfluxDB.
+
+    Examples:
+        $ measurement = 'serverroom'
+        $ tags = {'category': 'general', 'priority': 'high', 'owningParty': 'maintenace'}
+        $ field = {'humidity': 0.99} #get_sensor_humidity
+        
+        $ format_line_protocol(measurement,field,tags)
+
+        $ 'serverroom,category=general,priority=high,owningParty=maintenace humidity=0.99'
+    """
     str_output = f'{measurement}'
     if tags:
         for t in tags:
